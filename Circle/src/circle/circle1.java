06b43276f3a3cbaa1e76767cca144a90bfdca552
Deepadarshan <deepan.darshan@gmail.com>
2014-10-29 15:06:42 +0530

    init

---
+++ Circle/src/circle/circle1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package circle;
+
+/**
+ *
+ * @author Deepan
+ */
+public class circle1 {
+    
+    private double radius;
+    private final double pi=Math.PI;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public circle1(double radius) {
+        this.radius = radius;
+    }
+    
+    
+    
+    public double getArea(){
+        return radius*radius*pi;
+    }
+    
+    
+}
